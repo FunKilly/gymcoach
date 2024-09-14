91948a48cc7b2b4240ca3a4ebdfb7dad4de17f57
FunKilly <kowalczykoskar966@gmail.com>
2024-09-14 19:09:16 +0200

    add a test

--- tests/test_api/test_plans.py
+++ tests/test_api/test_plans.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.models.plans import Exercise
+from src.models.plans import Exercise, Plan
 from src.schemas.plans import PlanPost
 
 
@@ -16,11 +16,27 @@ async def exercises(db_session):
     return [ex1.id, ex2.id]
 
 
+@pytest.fixture
+async def plans(db_session, exercises):
+    ex1 = Exercise(name="deadlift", description="ex description")
+    ex2 = Exercise(name="squat", description="ex description")
+
+
+    plan1 = Plan(name="Test plan", description="plan for unit tests", exercises=[ex1,ex2])
+    db_session.add(plan1)
+    await db_session.commit()
+
+
 async def test_create_plan(client, exercises, db_session):
     body = PlanPost(
         name="Kozak plan", description="Plan to gain muscles", exercises=exercises
     )
 
     response = client.post("/plans", json=body.model_dump())
+
+    assert response.status_code == 200
+
+async def test_get_plans(client, db_session, plans):
+    response = client.get("/plans")
 
     assert response.status_code == 200
