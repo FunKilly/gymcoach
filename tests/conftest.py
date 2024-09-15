8d3cb918d45734bf6cba902c6fb54bb2871bc81a
FunKilly <kowalczykoskar966@gmail.com>
2024-09-15 11:34:11 +0200

    lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
-import pytest
 import os
+
+import pytest
 
 ENVIRONMENT_VARIABLES = {
     "POSTGRES_SERVER": "localhost",
     "POSTGRES_USER": "postgres",
     "POSTGRES_PASSWORD": "postgres",
     "POSTGRES_DB": "gymcoach-db",
-    }
+}
+
 
 @pytest.fixture(scope="session", autouse=True)
 def set_env():
