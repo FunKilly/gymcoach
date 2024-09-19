7114d22c02d7ef4a49fc40d0153a2910bb27d095
FunKilly <kowalczykoskar966@gmail.com>
2024-09-19 19:53:55 +0200

    connect workflows

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.17"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
-    }
-  }
-  backend "s3" {
-    region         = "eu-central-1"
-  }
-}
-# Configure the AWS Provider
-provider "aws" {
-  region = var.primary_region
-}
 
 
 data "terraform_remote_state" "first_configuration" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,18 @@ variable "primary_region" {
   type = string
   default = "eu-central-1"
 }
+
+variable "application_name" {
+  type = string
+  default = "gymcoach"
+}
+variable "environment_name" {
+  type = string
+  default = "sandbox"
+}
+
+
+variable "ecr_image_pushers" {
+  type = list(string)
+  default = ["gymcoach"]
+}
