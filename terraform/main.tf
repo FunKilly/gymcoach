73b4728984d2d37ea29471f1b626e2ff90674050
FunKilly <kowalczykoskar966@gmail.com>
2024-09-20 09:55:16 +0200

    add id to registry_endpoint echo

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,12 +56,12 @@ resource "aws_route_table" "public_route_table" {
 }
 
 # Associate Route Table with Public Subnet
-resource "aws_route_table_association" "public_subnet_association" {
+resource "aws_route_table_association" "public_subnet_association_2" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "public_subnet_association" {
+resource "aws_route_table_association" "public_subnet_association_1" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
