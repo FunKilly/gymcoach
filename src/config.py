7ae462079e5b7f7c97679ef71e0e6f7ef5a30373
FunKilly <kowalczykoskar966@gmail.com>
2024-09-29 21:32:32 +0200

    add latest tag to docker image

--- src/config.py
+++ src/config.py
@@ -4,6 +4,10 @@ from pydantic import PostgresDsn, computed_field
 from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+import logging
+log = logging.getLogger(__name__)
+
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -21,6 +25,7 @@ class Settings(BaseSettings):
     @computed_field  # type: ignore[prop-decorator]
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        log.error(f"port:{self.POSTGRES_PORT} server:{self.POSTGRES_SERVER} user:{self.self.POSTGRES_USER} password:{self.POSTGRES_PASSWORD} db:{self.POSTGRES_DB}")
         return MultiHostUrl.build(
             scheme="postgresql+asyncpg",
             username=self.POSTGRES_USER,
